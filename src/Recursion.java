3c7209a0dfc04978dffa32f1f54e8f3e40908df0
Jacob Boyles <jacob.boyles.16@cnu.edu>
2017-09-05 23:50:17 -0400

    Initial project fork
    Generated Javadoc
    Generated implemenation class with blank methods
    Generated test class
    Changed to Development branch

---
+++ src/Recursion.java
@@ -0,0 +1,35 @@
+/**
+ * Default comment, change this
+ * @author Jacob Boyles
+ * @version 1.0
+ */
+public class Recursion implements RecursionADT {
+
+    /* (non-Javadoc)
+     * @see RecursionADT#fact(java.lang.Integer)
+     */
+    @Override
+    public Integer fact(Integer n) throws IllegalArgumentException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see RecursionADT#isPal(java.lang.String)
+     */
+    @Override
+    public Boolean isPal(String s) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see RecursionADT#isInt(java.lang.String)
+     */
+    @Override
+    public Boolean isInt(String s) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
